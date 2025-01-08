ec134b9a4795a2f5044d150ea8ad56eba1ec7a99
sampathkumar2509 <sampathkumar2509@github.com>
2025-01-08 04:55:13 -0800

    sample merge

--- sos.sh
+++ sos.sh
@@ -7,7 +7,11 @@ echo "hi sampath"
 echo "hello monday"
 echo " i'm teja"
 echo " good morning"
+<<<<<<< HEAD
 sum=(( a + b ))
 echo "i'm ready to play a match"
 echo "haha"
+=======
+sum=$(( a +b ))
+>>>>>>> devops
 
