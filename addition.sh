3c2a7532baf2b7e3a91059cf5b53e07d743d6bc6
sampathkumar2509 <sampathkumar2509@github.com>
2025-01-07 22:47:44 -0800

    addition file

---
+++ addition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo " addition of two numbers "
+
+echo " enter the first number "
+
+num1=$1
+
+echo " enter the second number "
+
+num2=$2
+
+sum=$(( $1 + $2 ))
+
+echo " addition of two numbers is $sum "
+
+
